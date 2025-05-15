48a47fd0bd3419dd0ba59e9ec860f38b97eff90d
eriks <eriks@erik-inspiron3501>
2025-05-15 10:54:46 +0200

    test

--- ProgGame/src/main/Main.java
+++ ProgGame/src/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame window  = new JFrame("Shooter Game");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//window.setContentPane(new GamePanel());
+		window.pack();
+		window.setResizable(false);
+		window.setVisible(true);
+		window.pack();
 	}
 
 }
--- ProgGame/src/module-info.java
+++ ProgGame/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module ProgGame {
+	requires java.desktop;
 }
