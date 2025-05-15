54685ddbfac76d16cabc1c9d6d994288261343ed
BinaryOmnissiah <@>
2025-05-15 18:00:15 +0200

    first

--- ProgGame/src/main/Main.java
+++ ProgGame/src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame window  = new JFrame("Shooter Game");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //komm
 		window.setContentPane(new GamePanel());
 		window.pack();
 		window.setResizable(false);
